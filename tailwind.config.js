0978fde772d3da588f3472f98a89dca25be40e4d
ck1409 <chandankumarmr.ck@gmail.com>
2024-03-19 16:43:26 +0530

    F1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens :{
+      sm: '480px',
+      md: '798px',
+      lg: '976px',
+      xl: '1440px', 
+    },
+    extend: {
+      fontFamily:{
+      sans: ['poppins'],
+      }
+    },
+  },
+  plugins: [],
+}
